26c6f6d54a4c072346eb9ed17941a5780f3780fc
Kimberleydillon <kim.alexandra.dillon@gmail.com>
2018-01-04 10:34:26 -0800

    Refractor coffee app to include topping option for whipped cream.
    other refractoring as well included saving strings as resources.
    added else/if statment to ordersummary method to take in boolean value of the checked box.

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -4,11 +4,14 @@ package com.example.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 // This app display an order form for coffee.
 public class MainActivity extends AppCompatActivity {
     int numberOfCoffees = 0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +25,17 @@ public class MainActivity extends AppCompatActivity {
      */
 
     public void submitOrder(View view) {
-        String priceMessage = "Total: $" + (numberOfCoffees * 5);
-        displayMessage(priceMessage);
-        display(numberOfCoffees);
+        String priceMessage = calculatePrice() + " \n" + "Thank you!";
+        String order = createOrderSummary("Kim", numberOfCoffees, priceMessage);
+        TextView orderTextView = (TextView) findViewById(R.id.order_text_view);
+        orderTextView.setText(order);
     }
 
-    public void addQuantity(View view){
+    public void addQuantity(View view) {
         display(numberOfCoffees = numberOfCoffees + 1);
     }
 
-    public void minusQuantity(View view){
+    public void minusQuantity(View view) {
         numberOfCoffees = numberOfCoffees - 1;
         if (numberOfCoffees <= 0) {
             numberOfCoffees = 0;
@@ -39,19 +43,28 @@ public class MainActivity extends AppCompatActivity {
         display(numberOfCoffees);
     }
 
-
     /**
-     * This method displays the given quantity value on the screen.
+     * Order coffees with type and price included.
+     *
+     * @return total price
      */
+
+    private int calculatePrice() {
+        return numberOfCoffees * 5;
+    }
 
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_int);
         quantityTextView.setText("" + number);
     }
 
-    private void displayMessage( String priceMessage) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(priceMessage);
+
+    private String createOrderSummary(String name, int coffees, String total) {
+        boolean checked = ((CheckBox) findViewById(R.id.whipped_cream)).isChecked();
+        if (checked == true) {
+            return "Name:" + name + "\nQuantity:" + coffees + "\nWhipped Cream: Yes " + "\nTotal: $" + total;
+        } else {
+            return "Name:" + name + "\nQuantity:" + coffees + "\nWhipped Cream: No " + "\nTotal: $" + total;
+        }
     }
 }
-
